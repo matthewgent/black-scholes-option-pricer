f1177839f15d832db8484247071c35a75293ebd7
matthewgent <matthewgent1@gmail.com>
2025-08-06 14:39:38 +0100

    Moved the Black Scholes business logic to it's own module. Refactored code to conform to PEP8.

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
-import math
-from scipy.stats import norm
 import streamlit as st
-from abc import ABC, abstractmethod
+from black_scholes import PricingModel
 
-st.set_page_config(page_title="Black Scholes Option Pricer", page_icon=":chart_increasing:", layout="wide")
+st.set_page_config(
+    page_title="Black Scholes Option Pricer",
+    page_icon=":chart_increasing:",
+    layout="wide")
 
+# Sidebar
 st.sidebar.caption("Linkedin profile:")
-st.sidebar.page_link("https://linkedin.com/in/matthewgent", label="Matthew Gent", icon=":material/link:")
+st.sidebar.page_link(
+    "https://linkedin.com/in/matthewgent",
+    label="Matthew Gent",
+    icon=":material/link:")
 
 st.sidebar.divider()
 
@@ -57,84 +62,24 @@ risk_free_interest_rate_input = st.sidebar.number_input(
     width="stretch"
 )
 
+# Main page
 st.title("Black Scholes Option Pricer")
 
-class BlackScholesEnvironment:
-    __days_per_year: int = 365
+model = PricingModel(
+    spot_price_input, strike_price_input, days_to_maturity_input,
+    volatility_input, risk_free_interest_rate_input)
 
-    def __init__(self, spot_price: float, strike_price: float, days_to_maturity: float, volatility: float, risk_free_interest_rate: float):
-        self.spot_price = spot_price
-        self.strike_price = strike_price
-        self.days_to_maturity = days_to_maturity
-        self.volatility = volatility
-        self.risk_free_interest_rate = risk_free_interest_rate
-
-        self.years_to_maturity = days_to_maturity / self.__days_per_year
-        d1_numerator = math.log(spot_price / strike_price) + (risk_free_interest_rate + 0.5 * volatility ** 2) * self.years_to_maturity
-        d1_denominator = volatility * math.sqrt(self.years_to_maturity)
-        self.d1 = d1_numerator / d1_denominator
-        self.d2 = self.d1 - (volatility * math.sqrt(self.years_to_maturity))
-        self.d1_cdf = norm.cdf(self.d1)
-        self.d2_cdf = norm.cdf(self.d2)
-        self.d1_pdf = norm.pdf(self.d1)
-        self.d2_pdf = norm.pdf(self.d2)
-        self.continuous_discounting_factor = math.exp(-risk_free_interest_rate * self.years_to_maturity)
-
-        self.gamma = self.d1_pdf / (self.spot_price * self.volatility * math.sqrt(self.years_to_maturity))
-
-class Option(ABC):
-    env: BlackScholesEnvironment
-    price: float
-    delta: float
-    gamma: float
-
-    def __init__(self, env: BlackScholesEnvironment):
-        self.env = env
-        self.price = self.price()
-        self.delta = self.delta()
-        self.gamma = env.gamma
-        self.rho = self.rho()
-
-    @abstractmethod
-    def price(self) -> float:
-        pass
-
-    @abstractmethod
-    def delta(self) -> float:
-        pass
-
-class Call(Option):
-    def price(self) -> float:
-        return self.env.spot_price * self.env.d1_cdf - self.env.strike_price * self.env.continuous_discounting_factor * self.env.d2_cdf
-
-    def delta(self) -> float:
-        return self.env.d1_cdf
-
-    def rho(self) -> float:
-        return self.env.strike_price * self.env.years_to_maturity * self.env.continuous_discounting_factor * self.env.d2_cdf
-
-class Put(Option):
-    def price(self) -> float:
-        return self.env.strike_price * self.env.continuous_discounting_factor * (1 - self.env.d2_cdf) - self.env.spot_price * (1 - self.env.d1_cdf)
-
-    def delta(self) -> float:
-        return self.env.d1_cdf - 1
-
-    def rho(self) -> float:
-        return self.env.strike_price * self.env.years_to_maturity * self.env.continuous_discounting_factor * (self.env.d2_cdf - 1)
-
-environment = BlackScholesEnvironment(spot_price_input, strike_price_input, days_to_maturity_input, volatility_input, risk_free_interest_rate_input)
-call = Call(environment)
-put = Put(environment)
+# These greeks are identical in call and put so should only be calculated once
+gamma = model.call.gamma()
 
 call_column, put_column = st.columns(2)
 
-call_column.metric("CALL", f"€ {call.price:.2f}", border=True)
-call_column.badge("Δ = " + f"{call.delta:.3f}", color="blue")
-call_column.badge("Γ = " + f"{call.gamma:.3e}", color="green")
-call_column.badge("ρ = " + f"{call.rho:.3f}", color="orange")
+call_column.metric("CALL", f"€ {model.call.price():.2f}", border=True)
+call_column.badge("Δ = " + f"{model.call.delta():.3f}", color="blue")
+call_column.badge("Γ = " + f"{gamma:.3e}", color="green")
+call_column.badge("ρ = " + f"{model.call.rho():.3f}", color="orange")
 
-put_column.metric("PUT", f"€ {put.price:.2f}", border=True)
-put_column.badge("Δ = " + f"{put.delta:.3f}", color="blue")
-put_column.badge("Γ = " + f"{put.gamma:.3e}", color="green")
-put_column.badge("ρ = " + f"{put.rho:.3f}", color="orange")
+put_column.metric("PUT", f"€ {model.put.price():.2f}", border=True)
+put_column.badge("Δ = " + f"{model.put.delta():.3f}", color="blue")
+put_column.badge("Γ = " + f"{gamma:.3e}", color="green")
+put_column.badge("ρ = " + f"{model.put.rho():.3f}", color="orange")
---
+++ black_scholes.py
@@ -0,0 +1,105 @@
+from abc import ABC, abstractmethod
+from scipy.stats import norm
+import math
+
+
+class Option(ABC):
+    def __init__(
+            self, spot_price: float, strike_price: float,
+            years_to_maturity: float, volatility: float, d1_cdf: float,
+            d2_cdf: float, d1_pdf: float,
+            continuous_discounting_factor: float):
+        self.spot_price = spot_price
+        self.strike_price = strike_price
+        self.years_to_maturity = years_to_maturity
+        self.volatility = volatility
+        self.d1_cdf = d1_cdf
+        self.d2_cdf = d2_cdf
+        self.d1_pdf = d1_pdf
+        self.continuous_discounting_factor = continuous_discounting_factor
+
+    @abstractmethod
+    def price(self) -> float:
+        pass
+
+    @abstractmethod
+    def delta(self) -> float:
+        pass
+
+    @abstractmethod
+    def rho(self) -> float:
+        pass
+
+    # The following greeks have shared value for both call and put options
+    def gamma(self) -> float:
+        denominator = (self.spot_price
+                       * self.volatility
+                       * math.sqrt(self.years_to_maturity))
+        return self.d1_pdf / denominator
+
+
+class Call(Option):
+    def price(self) -> float:
+        return (self.spot_price
+                * self.d1_cdf
+                - self.strike_price
+                * self.continuous_discounting_factor
+                * self.d2_cdf)
+
+    def delta(self) -> float:
+        return self.d1_cdf
+
+    def rho(self) -> float:
+        return (self.strike_price
+                * self.years_to_maturity
+                * self.continuous_discounting_factor
+                * self.d2_cdf)
+
+
+class Put(Option):
+    def price(self) -> float:
+        return (self.strike_price * self.continuous_discounting_factor *
+                (1 - self.d2_cdf) - self.spot_price * (1 - self.d1_cdf))
+
+    def delta(self) -> float:
+        return self.d1_cdf - 1
+
+    def rho(self) -> float:
+        return (self.strike_price
+                * self.years_to_maturity
+                * self.continuous_discounting_factor
+                * (self.d2_cdf - 1))
+
+
+class PricingModel:
+    __days_per_year: int = 365
+    call: Call
+    put: Put
+
+    def __init__(
+            self, spot_price: float, strike_price: float,
+            days_to_maturity: float, volatility: float,
+            risk_free_interest_rate: float):
+        years_to_maturity = days_to_maturity / self.__days_per_year
+
+        d1_numerator = (math.log(spot_price / strike_price)
+                        + (risk_free_interest_rate + 0.5 * volatility ** 2)
+                        * years_to_maturity)
+        d1_denominator = volatility * math.sqrt(years_to_maturity)
+        d1 = d1_numerator / d1_denominator
+        d2 = d1 - (volatility * math.sqrt(years_to_maturity))
+
+        d1_cdf = norm.cdf(d1)
+        d2_cdf = norm.cdf(d2)
+        d1_pdf = norm.pdf(d1)
+
+        continuous_discounting_factor = math.exp(
+            -risk_free_interest_rate * years_to_maturity)
+
+        self.call = Call(
+            spot_price, strike_price, years_to_maturity, volatility, d1_cdf,
+            d2_cdf, d1_pdf, continuous_discounting_factor)
+
+        self.put = Put(
+            spot_price, strike_price, years_to_maturity, volatility, d1_cdf,
+            d2_cdf, d1_pdf, continuous_discounting_factor)
