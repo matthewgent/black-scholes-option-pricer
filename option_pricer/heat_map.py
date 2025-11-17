403c3d22e48ec52b4d421168ebee3d861ac34ae8
matthewgent <matthewgent1@gmail.com>
2025-11-17 09:17:40 +0000

    Implemented better use of NumPy with more appropriate type declarations.

--- option_pricer/heat_map.py
+++ option_pricer/heat_map.py
@@ -1,37 +1,47 @@
 import plotly.express as plotly
 from black_scholes import PricingModel
+import numpy as np
 
 
 def data(
-        x_prices: tuple[float],
-        y_volatilities: tuple[float],
+        x_prices: np.ndarray,
+        y_volatilities: np.ndarray,
         strike_price_input: float,
         days_to_maturity_input: float,
-        risk_free_interest_rate_input: float) -> (list, list):
-    call_data = []
-    put_data = []
-    for y_volatility in y_volatilities:
-        call_x_data = []
-        put_x_data = []
-        for x_price in x_prices:
-            pricing_model = PricingModel(
-                x_price, strike_price_input, days_to_maturity_input,
-                y_volatility, risk_free_interest_rate_input / 100
-            )
-            call_x_data.append(f"{pricing_model.call.price():.2f}")
-            put_x_data.append(f"{pricing_model.put.price():.2f}")
-        call_data.append(call_x_data)
-        put_data.append(put_x_data)
+        risk_free_interest_rate_input: float
+) -> (np.ndarray, np.ndarray):
+    string_format = '%.2f'
 
-    return call_data, put_data
+    def prices(price, volatility):
+        model = PricingModel(
+            price, strike_price_input, days_to_maturity_input,
+            volatility, risk_free_interest_rate_input / 100
+        )
+        return model.call.price(), model.put.price()
+
+    vectorized_prices = np.vectorize(
+        prices,
+        signature='(), () -> (), ()'
+    )
+    calls, puts = vectorized_prices(
+        x_prices[:, np.newaxis],
+        y_volatilities[np.newaxis, :]
+    )
+    call_strings = np.char.mod(string_format, calls)
+    put_strings = np.char.mod(string_format, puts)
+
+    return call_strings, put_strings
 
 
 def plot(
         st_column,
         title: str,
-        map_data: list,
-        x_prices,
-        y_volatilities) -> None:
+        map_data: np.ndarray,
+        x_prices: np.ndarray,
+        y_volatilities: np.ndarray
+) -> None:
+    tick_values = np.arange(1, x_prices.size)
+    print(map_data.shape)
     heat_map = plotly.imshow(
         map_data,
         text_auto=True,
@@ -46,12 +56,12 @@ def plot(
     heat_map.update_xaxes(
         tickangle=90,
         tickmode="array",
-        tickvals=list(range(len(x_prices))),
+        tickvals=tick_values,
         ticktext=[f"{price:.2f}" for price in x_prices]
     )
     heat_map.update_yaxes(
         tickmode="array",
-        tickvals=list(range(len(y_volatilities))),
+        tickvals=tick_values,
         ticktext=[f"{volatility:.2g}" for volatility in y_volatilities]
     )
     st_column.plotly_chart(heat_map, theme=None)
